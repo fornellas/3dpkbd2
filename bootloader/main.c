87376d0e57cef5be6327fbeb44d451cd168e7bab
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2019-11-09 18:16:45 +0000

    Simplify code

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,53 +1,42 @@
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
 #include <libopencm3/cm3/scb.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <pin_reset.h>
 
 #define SYSTEM_MEMORY_BASE 0x1FFF0000
+#define MAIN_MEMORY_BASE 0x08000000
+#define MAIN_MEMORY_SECTOR_0_SIZE 0x4000
 // Keep in sync with custom.devices.data
-#define MAIN_PROGRAM_BASE 0x08004000
+// The embedded DFU bootloader erases the whole Sector 0 when we program
+// any address of it. This forces us to offset the main program address to
+// Sector 1, so we don't erase the bootloader when we flash the main program.
+#define MAIN_PROGRAM_BASE (MAIN_MEMORY_BASE + MAIN_MEMORY_SECTOR_0_SIZE)
 
 void jump_to_addr(uint32_t addr);
 
 void jump_to_addr(uint32_t addr) {
-
-  // TODO AN2606 instructs for some cleanup to happen before jumping to
-  // system memory:
-  // 
-  // - Disable all peripheral clocks
-  // - Disable used PLL
-  // - Disable interrupts
-  // - Clear pending interrupts
-  // 
-  // However the code below should suffice as long as the bootloader
-  // code does not touch any of these.
-
-  // Set vector table base address.
-  SCB_VTOR = MAIN_PROGRAM_BASE;
-  // Initialize master stack pointer.
-  asm volatile("msr msp, %0"::"g"(*(volatile uint32_t *)addr));
-  // Jump to application.
-  ((void (*)(void)) *((uint32_t*) (addr + 4)))();
+	// Set vector table base address.
+	SCB_VTOR = addr;
+	// Initialize master stack pointer.
+	asm volatile("msr msp, %0"::"g"(*(volatile uint32_t *)addr));
+	// Jump to application.
+	((void (*)(void)) *((uint32_t*) (addr + 4)))();
 }
 
 int main(void) {
-  int pin_reset = (
-    (RCC_CSR & RCC_CSR_PINRSTF) // PIN reset flag
-    &&
-    !(
-      RCC_CSR & (
-        RCC_CSR_LPWRRSTF | // Low-power reset flag
-        RCC_CSR_WWDGRSTF | // Window watchdog reset flag
-        RCC_CSR_IWDGRSTF | // Independent watchdog reset flag
-        RCC_CSR_SFTRSTF | // Software reset flag
-        RCC_CSR_PORRSTF | // POR/PDR reset flag
-        RCC_CSR_BORRSTF // BOR reset flag
-      )
-    )
-  );
-
-  if(pin_reset) {
-    jump_to_addr(SYSTEM_MEMORY_BASE);
-  } else {
-    jump_to_addr(MAIN_PROGRAM_BASE);
-  }
+	if(PIN_RESET) {
+		// TODO AN2606 instructs for some cleanup to happen before jumping to
+		// system memory:
+		// 
+		// - Disable all peripheral clocks
+		// - Disable used PLL
+		// - Disable interrupts
+		// - Clear pending interrupts
+		// 
+		// However the code below should suffice as long as the bootloader
+		// code does not touch any of these.
+		jump_to_addr(SYSTEM_MEMORY_BASE);
+	} else {
+		jump_to_addr(MAIN_PROGRAM_BASE);
+	}
 }
