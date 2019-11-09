b10807c1cebe7e335d7724f4697845d76a38f56f
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2019-11-09 23:05:13 +0000

    Refactor build system

--- common.mk
+++ common.mk
@@ -1,28 +1,26 @@
+BUILD_DIR = bin
+
 SHARED_DIR = ../common
-CFILES += $(wildcard $(SHARED_DIR)/*.c)
+
+DEVICE=stm32f411ceu6
 VPATH += $(SHARED_DIR)
 INCLUDES += $(patsubst %,-I%, . $(SHARED_DIR))
 OPENCM3_DIR=../libopencm3
-PROJECT_DEVICES_DATA = ../custom.devices.data
-
-
-OPENCM3_DEVICES_DATA = ${OPENCM3_DIR}/ld/devices.data
-DEVICES_DATA=../generated.devices.data
-$(shell cat ${PROJECT_DEVICES_DATA} ${OPENCM3_DEVICES_DATA} > ${DEVICES_DATA})
 
 include $(OPENCM3_DIR)/mk/genlink-config.mk
+
 include ../rules.mk
+
+ifeq ($(CUSTOM_LDSCRIPT),)
 include $(OPENCM3_DIR)/mk/genlink-rules.mk
+else
+$(LDSCRIPT): $(CUSTOM_LDSCRIPT)
+	@printf "  GENLNK  $(DEVICE) ($<)\n"
+	$(Q)ln -s $< $@
+endif
 
-clean: clean_devices_data
-
-clean_devices_data:
-	rm -f ${DEVICES_DATA}
-
-.PHONY: clean_devices_data
-
-
-dfu: ${PROJECT}.bin
-	dfu-util --alt 0 --device 0483:df11 --dfuse-address ${DFUSE_ADDRESS}:leave --download ${PROJECT}.bin
+dfu: ${PROJECT}.bin $(LDSCRIPT)
+	@printf "  DFU-UTIL  $<\n"
+	$(Q)$(SHARED_DIR)/dfu-util-wrapper.sh $(LDSCRIPT) $(PROJECT).bin
 
 .PHONY: dfu
