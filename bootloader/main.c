17500dfb25b07df51d807e75259fff74d3697575
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2019-11-21 22:45:48 +0000

    Fix comment

--- bootloader/main.c
+++ bootloader/main.c
@@ -7,7 +7,7 @@
 #define SYSTEM_MEMORY_BASE 0x1FFF0000
 #define MAIN_MEMORY_BASE 0x08000000
 #define MAIN_MEMORY_SECTOR_0_SIZE 0x4000
-// Keep in sync with custom.devices.data
+// Keep in sync with keyboard/keyboard.ld
 // The embedded DFU bootloader erases the whole Sector 0 when we program
 // any address of it. This forces us to offset the main program address to
 // Sector 1, so we don't erase the bootloader when we flash the main program.
