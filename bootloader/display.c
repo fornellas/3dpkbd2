f79a214d22ad08695075b3ee7b3ce0c1eebdcaa4
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2019-12-03 23:00:26 +0000

    Tidy up DFU display

--- bootloader/display.c
+++ bootloader/display.c
@@ -48,12 +48,9 @@ static void draw(void) {
 				ucg_DrawBox(ucg, 0, 0, ucg_GetWidth(ucg), ucg_GetHeight(ucg));
 
 				ucg_SetColor(ucg, 0, 39, 39, 39);
-				ucg_SetScale2x2(ucg);
+				ucg_SetFont(ucg, ucg_font_helvB24_hf);
 				y_offset = ucg_GetFontAscent(ucg) + 2;
 				draw_centered_text("USB", y_offset);
-				ucg_UndoScale(ucg);
-
-				y_offset = y_offset + (ucg_GetHeight(ucg) - usb_height) / 2 + ucg_GetFontAscent(ucg) / 2;
 
 				ucg_DrawPixmap(
 					ucg,
@@ -65,14 +62,20 @@ static void draw(void) {
 				);
 			}
 
+			ucg_SetFont(ucg, ucg_font_helvB24_hf);
+			y_offset = ucg_GetFontAscent(ucg) + 2;
+			ucg_SetFont(ucg, ucg_font_helvB14_hf);
+			y_offset += (ucg_GetHeight(ucg) - y_offset - usb_height) / 2 + ucg_GetFontAscent(ucg) / 2;
+
 			switch(current_state.usbd_state) {
 				case USBD_STATE_RESET:
 					if(
 						current_state.usbd_state != last_state.usbd_state ||
 						current_state.force
 					){
-						ucg_SetColor(ucg, 0, 39, 39, 39);
-						draw_centered_text("Reset", y_offset);
+						ucg_SetColor(ucg, 0, 128, 128, 128);
+						ucg_SetFont(ucg, ucg_font_helvB14_hf);
+						draw_centered_text("reset", y_offset);
 					}
 					break;
 				case USBD_STATE_SUSPENDED:
@@ -80,8 +83,9 @@ static void draw(void) {
 						current_state.usbd_state != last_state.usbd_state ||
 						current_state.force
 					){
-						ucg_SetColor(ucg, 0, 39, 39, 39);
-						draw_centered_text("Suspended", y_offset);
+						ucg_SetColor(ucg, 0, 128, 128, 255);
+						ucg_SetFont(ucg, ucg_font_helvB14_hf);
+						draw_centered_text("suspended", y_offset);
 					}
 					break;
 				case USBD_STATE_ADDRESSED:
@@ -89,8 +93,9 @@ static void draw(void) {
 						current_state.usbd_state != last_state.usbd_state ||
 						current_state.force
 					){
-						ucg_SetColor(ucg, 0, 39, 39, 39);
-						draw_centered_text("Addressed", y_offset);
+						ucg_SetColor(ucg, 0, 128, 255, 128);
+						ucg_SetFont(ucg, ucg_font_helvB14_hf);
+						draw_centered_text("addressed", y_offset);
 					}
 					break;
 			}
@@ -106,12 +111,11 @@ static void draw(void) {
 				ucg_DrawBox(ucg, 0, 0, ucg_GetWidth(ucg), ucg_GetHeight(ucg));
 
 				ucg_SetColor(ucg, 0, 39, 39, 39);
-				ucg_SetScale2x2(ucg);
+				ucg_SetFont(ucg, ucg_font_helvB24_hf);
 				y_offset = ucg_GetFontAscent(ucg) + 2;
 				draw_centered_text("DFU", y_offset);
-				ucg_UndoScale(ucg);
 
-				y_offset += (ucg_GetHeight(ucg) - UFQFPN48_height ) / 2;
+				y_offset += (ucg_GetHeight(ucg) - y_offset - UFQFPN48_height) / 2;
 
 				ucg_DrawPixmap(
 					ucg,
@@ -122,40 +126,41 @@ static void draw(void) {
 					UFQFPN48_data
 				);
 
-				y_offset += UFQFPN48_height / 2;
+				ucg_SetFont(ucg, ucg_font_helvB14_hf);
+				y_offset += UFQFPN48_height / 2 + ucg_GetFontAscent(ucg) / 2 - 6;
+				ucg_SetColor(ucg, 0, 255, 255, 255);
 
-				// sprintf(buff, "dfu_status %d", current_state.dfu_status);
-				// draw_centered_text(buff);
-
-				switch(current_state.dfu_state) {
-					case STATE_DFU_IDLE:
-						break;
-					case STATE_DFU_DNLOAD_SYNC:
-					case STATE_DFU_DNLOAD_IDLE:
-						sprintf(buff, "%ldk", current_state.dfu_bytes / 1024);
-						ucg_SetScale2x2(ucg);
-						ucg_SetColor(ucg, 0, 255, 255, 255);
-						draw_centered_text(buff, y_offset / 2 + ucg_GetFontAscent(ucg) / 2);
-						ucg_UndoScale(ucg);
-						break;
-					case STATE_DFU_MANIFEST_SYNC:
-						ucg_SetColor(ucg, 0, 255, 255, 255);
-						draw_centered_text("Finished", y_offset + ucg_GetFontAscent(ucg) / 2);
-						break;
-					case STATE_DFU_ERROR:
-						ucg_SetColor(ucg, 0, 255, 255, 255);
-						draw_centered_text("Error", y_offset + ucg_GetFontAscent(ucg) / 2);
-						break;
-					default:
-						ucg_SetColor(ucg, 0, 255, 255, 255);
-						draw_centered_text("Unknown", y_offset + ucg_GetFontAscent(ucg) / 2);
-						break;
+				if(current_state.dfu_status == DFU_STATUS_OK) {
+					switch(current_state.dfu_state) {
+						case STATE_DFU_IDLE:
+							break;
+						case STATE_DFU_DNLOAD_SYNC:
+						case STATE_DFU_DNLOAD_IDLE:
+							sprintf(buff, "%ldkb", current_state.dfu_bytes / 1024);
+							draw_centered_text(buff, y_offset);
+							break;
+						case STATE_DFU_MANIFEST_SYNC:
+							ucg_SetColor(ucg, 0, 0, 255, 0);
+							draw_centered_text("Complete", y_offset);
+							break;
+						case STATE_DFU_ERROR:
+							ucg_SetColor(ucg, 0, 255, 0, 0);
+							draw_centered_text("Error!", y_offset);
+							break;
+						default:
+							ucg_SetColor(ucg, 0, 255, 0, 0);
+							draw_centered_text("Unknown", y_offset);
+							break;
+					}
+				} else {
+					draw_centered_text("Error!", y_offset);
 				}
 			}
 			break;
 		default:
 			ucg_ClearScreen(ucg);
-			ucg_SetColor(ucg, 0, 255, 255, 255);
+			ucg_SetColor(ucg, 0, 255, 0, 0);
+			ucg_SetFont(ucg, ucg_font_helvB14_hf);
 			draw_centered_text("USB Unknown", 0);
 			break;
 	}
@@ -200,7 +205,6 @@ void display_update(void) {
 		ucg_ClearScreen(ucg);
 
 		ucg_SetFontMode(ucg, UCG_FONT_MODE_TRANSPARENT);
-		ucg_SetFont(ucg, ucg_font_helvB10_hf);
 
 		draw();
 		first = 0;
