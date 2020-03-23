9347f95860fb56ed27e09e17681dc2acd269deb8
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2020-03-23 21:30:53 +0000

    Add SYSTICK_H

--- lib/systick.h
+++ lib/systick.h
@@ -1,3 +1,6 @@
+#ifndef SYSTICK_H
+#define SYSTICK_H
+
 #include <stdint.h>
 
 void systick_setup(void);
@@ -5,3 +8,5 @@ void systick_setup(void);
 void delay_ms(uint32_t);
 
 uint32_t uptime_ms(void);
+
+#endif
