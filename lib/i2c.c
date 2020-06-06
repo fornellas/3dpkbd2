16f7ffa55ce4ab6d8e1ff3371b858073adb96295
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2020-06-06 17:34:06 +0100

    Add error checknig for i2c_write

--- lib/i2c.c
+++ lib/i2c.c
@@ -11,18 +11,16 @@
 #define GPIO_SCL GPIO8
 #define GPIO_SDA GPIO9
 
-#define TIMEOUT_MS 1
+#define TIMEOUT_MS 2
 
-static void setup_io(void) {
+void i2c_setup(void) {
 	rcc_periph_clock_enable(RCC_GPIO_I2C);
 	rcc_periph_clock_enable(RCC_I2C);
 
 	gpio_mode_setup(GPIO_I2C, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_SCL | GPIO_SDA);
 	gpio_set_output_options(GPIO_I2C, GPIO_OTYPE_OD, GPIO_OSPEED_50MHZ, GPIO_SCL | GPIO_SDA);
 	gpio_set_af(GPIO_I2C, GPIO_AF4, GPIO_SCL | GPIO_SDA);
-}
 
-static void setup_peripheral(void) {
 	i2c_peripheral_disable(I2C);
 
 	i2c_reset(I2C);
@@ -38,125 +36,103 @@ static void setup_peripheral(void) {
 	i2c_set_own_7bit_slave_address(I2C, 0x00);
 }
 
-void i2c_setup(void) {
-	setup_io();
-	setup_peripheral();
-}
-
-static uint8_t wait_not_busy(void) {
-	uint32_t start_time;
-
-	start_time = uptime_ms();
-	while ((I2C_SR2(I2C) & I2C_SR2_BUSY)) {
-		if(uptime_ms() - start_time >= TIMEOUT_MS){
-			setup_peripheral();
-			return 1;
-		}
-	}
-	return 0;
-}
-
-static uint8_t wait_for_master_mode_selected(void) {
-	uint32_t start_time;
-
-	start_time = uptime_ms();
-	while (
-		!(
-			(I2C_SR1(I2C) & I2C_SR1_SB)
-			& (
-				I2C_SR2(I2C) & (I2C_SR2_MSL | I2C_SR2_BUSY )
+static uint8_t abort_if_error_condition(uint32_t start_ms) {
+	if(
+		(uptime_ms() - start_ms >= TIMEOUT_MS)
+		|| (
+			I2C_SR1(I2C) & (
+				I2C_SR1_BERR |
+				I2C_SR1_ARLO |
+				I2C_SR1_AF |
+				I2C_SR1_TIMEOUT
 			)
 		)
 	) {
-		if(uptime_ms() - start_time >= TIMEOUT_MS){
-			setup_peripheral();
-			return 1;
-		}
-	}
-	return 0;
-}
-
-static uint8_t wait_for_address_transfer(void) {
-	uint32_t start_time;
-
-	start_time = uptime_ms();
-	while (!(I2C_SR1(I2C) & I2C_SR1_ADDR)) {
-		if(uptime_ms() - start_time >= TIMEOUT_MS){
-			setup_peripheral();
-			return 1;
-		}
-	}
-	return 0;
-}
-
-uint8_t i2c_write(uint8_t addr, uint8_t *w, size_t wn) {
-	if(wait_not_busy())
+		i2c_setup();
 		return 1;
+	}
 
+	return 0;
+}
+
+uint8_t i2c_write(uint8_t addr, uint8_t *data, size_t len) {
+	uint32_t start_ms;
+
+	// Ensure lines are not busy
+	if((I2C_SR2(I2C) & I2C_SR2_BUSY)){
+		i2c_setup();
+		return 1;
+	}
+
+	// Send start condition
+	start_ms = uptime_ms();
 	i2c_send_start(I2C);
+	while (!(I2C_SR2(I2C) & I2C_SR2_BUSY))
+		if(abort_if_error_condition(start_ms))
+			return 1;
+	while(!(I2C_SR2(I2C) & I2C_SR2_MSL))
+		if(abort_if_error_condition(start_ms))
+			return 1;
+	while(!(I2C_SR1(I2C) & I2C_SR1_SB))
+		if(abort_if_error_condition(start_ms))
+			return 1;
 
-	if(wait_for_master_mode_selected())
-		return 1;
-
+	// Send slave address
+	start_ms = uptime_ms();
 	i2c_send_7bit_address(I2C, addr, I2C_WRITE);
-
-	if(wait_for_address_transfer())
-		return 1;
-
-	/* Clearing ADDR condition sequence. */
+	while (!(I2C_SR1(I2C) & I2C_SR1_ADDR))
+		if(abort_if_error_condition(start_ms))
+			return 1;;
 	(void)I2C_SR2(I2C);
 
-	for (size_t i = 0; i < wn; i++) {
-		uint32_t start_time;
-
-		i2c_send_data(I2C, w[i]);
-		start_time = uptime_ms();
-		while (
-			!(
-				I2C_SR1(I2C) & (I2C_SR1_BTF)
-			)
-		) {
-			if(uptime_ms() - start_time >= TIMEOUT_MS){
-				setup_peripheral();
+	// Send data
+	for (size_t i = 0; i < len; i++) {
+		start_ms = uptime_ms();
+		i2c_send_data(I2C, data[i]);
+		while (!(I2C_SR1(I2C) & (I2C_SR1_BTF)))
+			if(abort_if_error_condition(start_ms))
 				return 1;
-			}
-		}
 	}
 
+	// Send stop
 	i2c_send_stop(I2C);
-
 	return 0;
 }
 
-uint8_t i2c_read(uint8_t addr, uint8_t *r, size_t rn) {
+uint8_t i2c_read(uint8_t addr, uint8_t *data, size_t len) {
+	uint32_t start_ms;
+
 	i2c_send_start(I2C);
+
 	i2c_enable_ack(I2C);
 
-	if(wait_for_master_mode_selected())
-		return 1;
+	/* Wait for master mode selected */
+	while (!((I2C_SR1(I2C) & I2C_SR1_SB) & (I2C_SR2(I2C) & (I2C_SR2_MSL | I2C_SR2_BUSY))));
 
+	start_ms = uptime_ms();
 	i2c_send_7bit_address(I2C, addr, I2C_READ);
-
-	if(wait_for_address_transfer())
-		return 1;
+	while (!(I2C_SR1(I2C) & I2C_SR1_ADDR)) {
+		if(uptime_ms() - start_ms >= TIMEOUT_MS){
+			i2c_send_stop(I2C);
+			return 1;
+		}
+	};
 
 	/* Clearing ADDR condition sequence. */
 	(void)I2C_SR2(I2C);
 
-	for (size_t i = 0; i < rn; ++i) {
-		uint32_t start_time;
-
-		if (i == rn - 1) {
+	for (size_t i = 0; i < len; ++i) {
+		if (i == len - 1) {
 			i2c_disable_ack(I2C);
 		}
-		start_time = uptime_ms();
+		start_ms = uptime_ms();
 		while (!(I2C_SR1(I2C) & I2C_SR1_RxNE)) {
-			if(uptime_ms() - start_time >= TIMEOUT_MS){
-				setup_peripheral();
+			if(uptime_ms() - start_ms >= TIMEOUT_MS){
+				i2c_send_stop(I2C);
 				return 1;
 			}
 		}
-		r[i] = i2c_get_data(I2C);
+		data[i] = i2c_get_data(I2C);
 	}
 	i2c_send_stop(I2C);
 
--- lib/i2c.h
+++ lib/i2c.h
@@ -7,7 +7,7 @@
 #define I2C I2C1
 
 void i2c_setup(void);
-uint8_t i2c_write(uint8_t addr, uint8_t *w, size_t wn);
-uint8_t i2c_read(uint8_t addr, uint8_t *r, size_t rn);
+uint8_t i2c_write(uint8_t addr, uint8_t *data, size_t len);
+uint8_t i2c_read(uint8_t addr, uint8_t *data, size_t len);
 
 #endif
